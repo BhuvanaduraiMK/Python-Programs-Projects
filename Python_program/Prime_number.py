00dc465de57696ed490a919c9fa92d63060171b0
BhuvanaduraiMK <puvi10s1806@gmail.com>
2025-08-14 18:47:05 +0530

    Added

---
+++ Python_program/Prime_number.py
@@ -0,0 +1,15 @@
+num = int(input("enter a value: "))
+flag = True
+if num == 0 or num == 1:
+    print(f'{num} is not a prime number')
+elif num>1:
+    for i in range(2,num):
+        if num%i==0:
+            flag = False
+            break
+    if flag:
+        print(num," Prime number")
+    else:
+        print(num," Not Prime number")
+else:
+    print(f"{num} is not  prime number")
---
+++ Python_program/Sum_of_n_digits.py
@@ -0,0 +1,5 @@
+user1 = int(input('Enter a n digits of number:'))
+sum1 = 0
+for i in range(1,user1+1):
+    sum1 += i
+print(sum1)
